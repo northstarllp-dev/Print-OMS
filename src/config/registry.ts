9253819b6237df720eca34ba0c4959744db6dbae
Your New Name <northstarllp.dev@gmail.com>
2026-08-01 19:30:22 +0530

    add printsquirel client

--- src/config/registry.ts
+++ src/config/registry.ts
@@ -3,10 +3,12 @@ import { theBoardCompanyConfig } from "./clients/the-board-company";
 import { printecConfig } from "./clients/printec";
 import { hitechVisionConfig } from "./clients/hitech-vision";
 import { defaultConfig as printomsConfig } from "./clients/printoms";
+import { printsquirelConfig } from "./clients/printsquirel";
 
 export const clientRegistry: Record<string, Partial<PrintOMSClientConfig>> = {
   "the-board-company": theBoardCompanyConfig,
   "printec": printecConfig,
   "hitech-vision": hitechVisionConfig,
   "printoms": printomsConfig,
+  "printsquirel": printsquirelConfig,
 };
